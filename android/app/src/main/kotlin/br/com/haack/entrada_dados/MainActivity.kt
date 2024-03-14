4ea75b9948a50bc61a1296cda3b31e45741baadd
Giancarlo Haack Albano <gian418@gmail.com>
2024-03-13 21:03:02 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/haack/entrada_dados/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.haack.entrada_dados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Álcool ou Gasolina"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Álcool, ex: 1.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.76"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(15),
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
